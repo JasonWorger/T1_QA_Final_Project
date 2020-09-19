a14938e334dcaec4e238d81efae1b810ab2144de
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-19 11:49:52 +0100

    FP-64 #comment ran Terraform fmt

--- Terraform FP-T1/Terraform Branch Model/modules/infrastructure/EKS/main.tf
+++ Terraform FP-T1/Terraform Branch Model/modules/infrastructure/EKS/main.tf
@@ -18,7 +18,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 # Add EKS Node Group to Cluster.
 
 resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster
+  cluster_name    = var.cluster_name
   node_group_name = "FP-T1_${var.environment}_Node_Group"
   node_role_arn   = var.eks_node_role_arn
   subnet_ids      = [var.private_eks_subnet_id, var.public_subnet_id]
