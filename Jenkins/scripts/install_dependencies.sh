cf8849d6ef30f476e55adcb977da5481210fb9f4
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-21 12:34:04 +0100

    FP-64 #comment Added installer for AWS CLI

--- Jenkins/scripts/install_dependencies.sh
+++ Jenkins/scripts/install_dependencies.sh
@@ -2,7 +2,13 @@
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install -y wget unzip
+sudo apt install -y wget unzip curl
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
 
 wget https://releases.hashicorp.com/terraform/0.13.3/terraform_0.13.3_linux_amd64.zip
 
