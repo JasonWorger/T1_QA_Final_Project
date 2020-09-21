7c581105815efacb0b0420feefdce3a9a298f6e9
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-21 12:42:22 +0100

    FP-64 #comment Added installer for AWS CLI

--- Jenkins/scripts/aws_cli_config.sh
+++ Jenkins/scripts/aws_cli_config.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set region eu-west-1
+aws configure set output json
