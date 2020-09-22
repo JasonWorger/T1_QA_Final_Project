a8a917c6037b3a0d9d2066cd74b1801434760d30
Samuel Asquith <samuelasquith@btinternet.com>
2020-09-22 14:33:54 +0100

    Update artifact.sh

--- Jenkins/scripts/docker/artifact.sh
+++ Jenkins/scripts/docker/artifact.sh
@@ -3,40 +3,40 @@
 # cd into docker repository.
 
 cd docker
-
+echo "cd into docker"
 # Remove our previous images.
 docker-compose down --rmi all
-
+echo "Removed images"
 # Clone Angular Repository.
 
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
-
+echo "cloned angular"
 # Clone REST Repository.
 
 git clone https://github.com/spring-petclinic/spring-petclinic-rest.git
-
+echo "cloned rest"
 # Copy Dockerfile from Dockerfiles/PetClinicREST to REST cloned folder.
 
 sudo cp Dockerfiles/PetClinicREST/Dockerfile spring-petclinic-rest
-
+echo  "copied rest dockerfile"
 # Copy Dockerfile from Dockerfiles/PetClinicAngular to Angular cloned folder.
 
 sudo cp Dockerfiles/PetClinicAngular/Dockerfile spring-petclinic-angular
 sudo cp Dockerfiles/PetClinicAngular/.dockerignore spring-petclinic-angular
 
 # Run docker-compose.
-
+echo "About to build containers"
 docker-compose build
-
+echo "Built containers"
 # Login with our docker credentials.
 
 sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-
+echo "Logged in"
 # Push our built images to docker
 
 sudo docker-compose push
-
+echo "Pushed containers"
 # Remove our cloned git repos to save space.
 
-sudo rm -r spring-petclinic-angular
-sudo rm -r spring-petclinic-rest
+sudo rm -rf spring-petclinic-angular
+sudo rm -rf spring-petclinic-rest
