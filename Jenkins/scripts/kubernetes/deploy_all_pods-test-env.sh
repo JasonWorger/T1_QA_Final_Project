42dc57344203b6deef52e7931c12d7987bc91aea
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-22 16:07:17 +0100

    Completed SQL Configuration Script - Just need to export endpoint into file.

--- Jenkins/scripts/kubernetes/deploy_all_pods-test-env.sh
+++ Jenkins/scripts/kubernetes/deploy_all_pods-test-env.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # Installs the CloudWatch Agent and FluentD pod requirements for container insights.
-
 curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluentd-quickstart.yaml | sed "s/{{cluster_name}}/testing_FP_T1_Cluster/;s/{{region_name}}/eu-west-1/" | kubectl apply -f -
