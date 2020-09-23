9374c5393a672dca40e3860651e0cc026557e568
Samuel Asquith <samuelasquith@btinternet.com>
2020-09-23 13:28:22 +0000

    I'm an idiot

--- Jenkins/scripts/test_software.sh
+++ Jenkins/scripts/test_software.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd spring-petclinic-angular
-sudo apt install openjdk-8-jre maven default-jre
-npm install
-ng update
-npm update
-npm i karma karma-chrome-launcher chromium
-export CHROME_BIN="/usr/local/lib/node_modules/chromium"
-ng test
+#sudo apt install openjdk-8-jre maven default-jre
+#npm install
+#ng update
+#npm update
+#npm i karma karma-chrome-launcher chromium
+#export CHROME_BIN="/usr/local/lib/node_modules/chromium"
+#ng test
+mvn test
