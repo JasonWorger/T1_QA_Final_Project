08e9f21f812ce0228935dcc9474135fac35a0fd8
Samuel Asquith <samuelasquith@btinternet.com>
2020-09-23 11:34:32 +0000

    testing revision1

--- Jenkins/scripts/test_software.sh
+++ Jenkins/scripts/test_software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 cd spring-petclinic-angular
-sudo apt install install openjdk-8-jre maven default-jre
+sudo apt install openjdk-8-jre maven default-jre
 npm i karma
 ng test
