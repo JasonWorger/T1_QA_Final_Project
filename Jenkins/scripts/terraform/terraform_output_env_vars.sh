6f332faebf9e02bbeb82e6f72c532638bcd8ccf9
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-23 14:39:36 +0100

    Added Dockerfile for nginx

--- Jenkins/scripts/terraform/terraform_output_env_vars.sh
+++ Jenkins/scripts/terraform/terraform_output_env_vars.sh
@@ -8,8 +8,6 @@ variable=$()
 
 echo "exporting the terraform variable..."
 
-export TESTING_RDS_ENDPOINT=`terraform output rds_endpoint`
-
 echo " The env variable TESTING_RDS_ENDPOINT IS..."
 
 echo $TESTING_RDS_ENDPOINT
--- Jenkins/scripts/terraform/terraform_testing_env.sh
+++ Jenkins/scripts/terraform/terraform_testing_env.sh
@@ -12,4 +12,13 @@ terraform plan --var-file=test.tfvars -out=testplan
 
 terraform apply "testplan"
 
+echo "Running terraform output..."
+
 terraform output rds_endpoint
+
+echo "Ran terraform output..."
+echo "Exporting environment variable..."
+
+export TESTING_RDS_ENDPOINT=`terraform output rds_endpoint`
+
+echo "Environment variable exported..."
