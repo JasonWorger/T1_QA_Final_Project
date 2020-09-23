0cd2b68822e462f6e1611f4b1b50a52ca7e4eec6
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-23 17:04:12 +0100

    commented out test stage to save time.

--- Jenkins/scripts/terraform/terraform_testing_env.sh
+++ Jenkins/scripts/terraform/terraform_testing_env.sh
@@ -12,23 +12,8 @@ terraform plan --var-file=test.tfvars -out=testplan
 
 terraform apply "testplan"
 
-# echo "Running terraform output..."
+echo "Creating new text file from terraform output..."
 
-#terraform output rds_endpoint
+terraform output rds_endpoint >> testing_rds_endpoint.txt
 
-# echo "Ran terraform output..."
-
-# echo "Creating new text file from terraform output..."
-
-#terraform output rds_endpoint >> testing_rds_endpoint.txt
-
-# echo "Text file created."
-
-# echo "Creating environment variable from contents of rds_endpoint.txt..."
-
-#export TESTING_RDS_ENDPOINT=$(terraform output rds_endpoint)
-
-
-#echo $TESTING_RDS_ENDPOINT
-
-# sudo rm testing_rds_endpoint.txt
+echo "Text file created."
