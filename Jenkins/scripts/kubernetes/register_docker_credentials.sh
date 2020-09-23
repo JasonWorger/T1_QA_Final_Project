8ebc313e190c4ecebd828d8f8c7c748a87237f4c
Josh Higginson <joshuahigginson1@gmail.com>
2020-09-23 12:58:12 +0100

    Changed File Permissions

--- Jenkins/scripts/kubernetes/register_docker_credentials.sh
+++ Jenkins/scripts/kubernetes/register_docker_credentials.sh
@@ -1,3 +1,3 @@
 # Create our kubernetes secret.
 
-kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
+kubectl apply secret docker-registry dockercredentials --docker-server=https://index.docker.io/v1/ --docker-username=$DOCKER_USERNAME --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
