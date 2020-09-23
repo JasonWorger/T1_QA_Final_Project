12496bc786cc67e1a3b42cd36efc87d8dd830854
Samuel Asquith <samuelasquith@btinternet.com>
2020-09-23 13:19:00 +0000

    Chromium addition

--- Jenkins/scripts/test_software.sh
+++ Jenkins/scripts/test_software.sh
@@ -4,5 +4,6 @@ sudo apt install openjdk-8-jre maven default-jre
 npm install
 ng update
 npm update
-npm i karma karma-chrome-launcher
+npm i karma karma-chrome-launcher chromium
+export CHROME_BIN="/usr/local/lib/node_modules/chromium"
 ng test
